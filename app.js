dec9c07b93f182c8d9da5180646dbc1730a62316
kashtan dariya <kashtandariya@gmail.com>
2024-03-18 18:07:55 +0100

    add: stampa numeri da 1 a 100

---
+++ app.js
@@ -0,0 +1,12 @@
+// scrivi un programma che stampi in console i numeri da 1 a 100
+// Successivamente aggiungi un controllo sul numero in modo tale che:
+// 1. per i multipli di 3 stampi “Fizz” al posto del numero
+// 2. per i multipli di 5 stampi “Buzz” al posto del numero.
+// 3. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+// 4. in tutti gli altri casi venga stampato il numero stesso
+
+
+//stampa numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+        console.log(i);
+    }
